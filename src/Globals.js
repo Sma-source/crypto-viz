9c8835d62b79b6bd0ff1916c9c7a49665ae8f04a
Sma-source <rabhi.smain@gmail.com>
2021-01-04 22:38:28 +0100

    add index

--- src/Globals.js
+++ src/Globals.js
@@ -1,14 +1,14 @@
 import React from "react";
-const Globals = ({ globals }) => {
+const Globals = ({ globals, index }) => {
   return (
     <>
-      {globals.map((global, index) => {
+      {globals.map((global) => {
         const { total_mcap, mcap_change } = global;
         return (
           <>
-            <div className="container" key={index}>
+            <div className="container">
               <h1>Today's Cryptocurrency Prices by Market Cap</h1>
-              <h3>
+              <h3 key={index}>
                 The global crypto market cap is $
                 {Math.trunc(total_mcap / 1000000000)}B, a {mcap_change}%
                 {mcap_change > 0 ? " increase" : " decrease"} over the last day
