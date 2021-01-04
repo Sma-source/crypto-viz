c27b3838e74dc4c9e9803369e65f7142fdd7351a
Sma-source <rabhi.smain@gmail.com>
2021-01-04 22:38:59 +0100

    import globals component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Loading from "./Loading";
 import Coins from "./Coins";
+import Globals from "./Globals";
 
 const App = () => {
   const [coins, setCoins] = useState([]);
+  const [globals, setGlobals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const globalUrl = "https://api.coinlore.net/api/global/";
   const url = "https://api.coinlore.net/api/tickers/?limit=15";
   const getCoins = async () => {
     try {
@@ -20,10 +23,24 @@ const App = () => {
       console.log(error);
     }
   };
+
+  const getGlobals = async () => {
+    try {
+      const res = await fetch(globalUrl);
+      const donne = await res.json();
+      const item = donne;
+      setGlobals(item);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
     getCoins();
+    getGlobals();
+
     const interval = setInterval(() => {
       getCoins();
+      getGlobals();
     }, 30000);
 
     return () => clearInterval(interval);
@@ -34,6 +51,7 @@ const App = () => {
   }
   return (
     <>
+      <Globals globals={globals} />
       <Coins coins={coins} />
     </>
   );
