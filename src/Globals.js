92e57495f554a9639ecc2e5074a256a71ed9aafa
Sma-source <rabhi.smain@gmail.com>
2021-01-05 16:58:36 +0100

    add updown component

--- src/Globals.js
+++ src/Globals.js
@@ -1,4 +1,5 @@
 import React from "react";
+import UpDown from "./UpDown";
 const Globals = ({ globals }) => {
   return (
     <div className="container">
@@ -8,8 +9,9 @@ const Globals = ({ globals }) => {
         return (
           <h3 key={total_mcap}>
             The global crypto market cap is{" "}
-            {Math.trunc(total_mcap / 1000000000)}B, a {mcap_change}%{" "}
-            {mcap_change > 0 ? "increase" : "decrease"} over the last day{" "}
+            {Math.trunc(total_mcap / 1000000000)}B, a{" "}
+            <UpDown value={mcap_change} />
+            {mcap_change > 0 ? " increase" : " decrease"} over the last day{" "}
           </h3>
         );
       })}
