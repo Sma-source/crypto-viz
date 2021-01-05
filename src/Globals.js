2c612941e4580053d2da735d3a8ffd41b3d03243
Sma-source <rabhi.smain@gmail.com>
2021-01-05 15:57:37 +0100

    fix by adding key

--- src/Globals.js
+++ src/Globals.js
@@ -1,24 +1,19 @@
 import React from "react";
-const Globals = ({ globals, index }) => {
+const Globals = ({ globals }) => {
   return (
-    <>
+    <div className="container">
+      <h1>Today's Cryptocurrency Prices by Market Cap</h1>
       {globals.map((global) => {
         const { total_mcap, mcap_change } = global;
         return (
-          <>
-            <div className="container">
-              <h1>Today's Cryptocurrency Prices by Market Cap</h1>
-              <h3 key={index}>
-                The global crypto market cap is $
-                {Math.trunc(total_mcap / 1000000000)}B, a {mcap_change}%
-                {mcap_change > 0 ? " increase" : " decrease"} over the last day
-              </h3>
-            </div>
-          </>
+          <h3 key={total_mcap}>
+            The global crypto market cap is{" "}
+            {Math.trunc(total_mcap / 1000000000)}B, a {mcap_change}%{" "}
+            {mcap_change > 0 ? "increase" : "decrease"} over the last day{" "}
+          </h3>
         );
       })}
-    </>
+    </div>
   );
 };
-
 export default Globals;
