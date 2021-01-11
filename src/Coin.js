92180ee36434401c863205c6f391b990f526143d
Sma-source <rabhi.smain@gmail.com>
2021-01-11 21:35:40 +0100

    modify link add classname

--- src/Coin.js
+++ src/Coin.js
@@ -14,33 +14,31 @@ const Coin = ({
   csupply,
 }) => {
   return (
-    <Link to={`/coins/${coin.id}`}>
-      <div className="row" key={id}>
-        <div className="cell" data-title="#">
-          {rank}
-        </div>
-        <div className="cell" data-title="Name">
-          {name}
-        </div>
-        <div className="cell" data-title="Price">
-          ${price_usd}
-        </div>
-        <div className="cell" data-title="24h">
-          <UpDown value={percent_change_24h}></UpDown>
-        </div>
-        <div className="cell" data-title="7h">
-          <UpDown value={percent_change_7d}></UpDown>
-        </div>
+    <Link to={`/coins/${id}`} className="row" key={id}>
+      <div className="cell" data-title="#">
+        {rank}
+      </div>
+      <div className="cell" data-title="Name">
+        {name}
+      </div>
+      <div className="cell" data-title="Price">
+        ${price_usd}
+      </div>
+      <div className="cell" data-title="24h">
+        <UpDown value={percent_change_24h}></UpDown>
+      </div>
+      <div className="cell" data-title="7h">
+        <UpDown value={percent_change_7d}></UpDown>
+      </div>
 
-        <div className="cell" data-title="Market Cap">
-          ${market_cap_usd}
-        </div>
-        <div className="cell" data-title="Volume">
-          ${volume24a}
-        </div>
-        <div className="cell" data-title="Circulation Supply">
-          {csupply} {symbol}
-        </div>
+      <div className="cell" data-title="Market Cap">
+        ${market_cap_usd}
+      </div>
+      <div className="cell" data-title="Volume">
+        ${volume24a}
+      </div>
+      <div className="cell" data-title="Circulation Supply">
+        {csupply} {symbol}
       </div>
     </Link>
   );
