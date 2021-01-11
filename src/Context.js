89dc71318ebb67b1a89826fb6fc0301430cdaa74
Sma-source <rabhi.smain@gmail.com>
2021-01-11 22:05:44 +0100

    add details api url

--- src/Context.js
+++ src/Context.js
@@ -5,9 +5,10 @@ const GlobalContext = React.createContext();
 const GlobalProvider = ({ children }) => {
   const globalUrl = "https://api.coinlore.net/api/global/";
   const url = "https://api.coinlore.net/api/tickers/?limit=15";
+  const detailsUrl = " https://api.coinlore.net/api/ticker/";
 
   return (
-    <GlobalContext.Provider value={{ globalUrl, url }}>
+    <GlobalContext.Provider value={{ globalUrl, url, detailsUrl }}>
       {children}
     </GlobalContext.Provider>
   );
