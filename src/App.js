6899c13b813a0867f5df07995dc680bc227a52d2
Sma-source <rabhi.smain@gmail.com>
2021-01-11 21:22:14 +0100

    create route

--- src/App.js
+++ src/App.js
@@ -1,48 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Details from "./pages/Details";
 
 import "./App.css";
-import Loading from "./Loading";
-import Coins from "./Coins";
-import Globals from "./Globals";
+
 import { GlobalProvider } from "./Context";
-import DomCoin from "./DomCoin";
 
 const App = () => {
-  const [coins, setCoins] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const url = "https://api.coinlore.net/api/tickers/?limit=15";
-  const getCoins = async () => {
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      const item = data.data;
-      // console.log(item);
-      setIsLoading(false);
-      setCoins(item);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getCoins();
-
-    const interval = setInterval(() => {
-      getCoins();
-    }, 30000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  if (isLoading) {
-    return <Loading />;
-  }
   return (
-    <GlobalProvider>
-      <DomCoin />
-      <Globals />
-      <Coins coins={coins} />
-    </GlobalProvider>
+    <BrowserRouter>
+      <GlobalProvider>
+        <Route exact path="/coins" component={Home} />
+      </GlobalProvider>
+    </BrowserRouter>
   );
 };
 
