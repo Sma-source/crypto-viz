c9e99627f3c91b1f7a96a22afe4f0d5b32952282
Sma-source <rabhi.smain@gmail.com>
2021-01-11 22:06:30 +0100

    render details by map over

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useGlobalContext } from "../Context";
 const Details = () => {
-  return <></>;
+  const { detailsUrl } = useGlobalContext();
+  const { id } = useParams();
+  const [details, setDetails] = useState([]);
+  useEffect(() => {
+    const getDetails = async () => {
+      try {
+        const res = await fetch(`${detailsUrl}/?id=${id}`);
+        const data = await res.json();
+        const item = data;
+        setDetails(item);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getDetails();
+
+    const interval = setInterval(() => {
+      getDetails();
+    }, 30000);
+
+    return () => clearInterval(interval);
+  }, [id, detailsUrl]);
+
+  return (
+    <>
+      {details.map((detail) => {
+        const { id, name, price_usd } = detail;
+        return (
+          <div key={id}>
+            <h3>{name} </h3>
+            <h4>{price_usd} </h4>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 export default Details;
