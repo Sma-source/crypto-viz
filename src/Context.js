ba75f289b0fe03cb195d3e8470737f1e3b357cfe
Sma-source <rabhi.smain@gmail.com>
2021-01-12 17:23:30 +0100

    change: api

--- src/Context.js
+++ src/Context.js
@@ -4,7 +4,8 @@ const GlobalContext = React.createContext();
 
 const GlobalProvider = ({ children }) => {
   const globalUrl = "https://api.coinlore.net/api/global/";
-  const url = "https://api.coinlore.net/api/tickers/?limit=15";
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
   const detailsUrl = " https://api.coinlore.net/api/ticker/";
 
   return (
