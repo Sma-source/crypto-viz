208ced9d99a898b3eeb34a4dd4324a2728fea5c8
Sma-source <rabhi.smain@gmail.com>
2021-01-21 21:29:45 +0100

    update detailschart.js: modify chart

--- src/DetailsChart.js
+++ src/DetailsChart.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect, useState } from "react";
 import Chartjs from "chart.js";
+import { Line } from "@reactchartjs/react-chart.js";
 import UpDown from "./UpDown";
 
 const DetailsChart = ({ data }) => {
@@ -7,78 +8,113 @@ const DetailsChart = ({ data }) => {
   const { day, week, year, detail } = data;
   const [timeFormat, setTimeFormat] = useState("24h");
 
-  // const determineTimeFormat = () => {
-  //   switch (timeFormat) {
-  //     case "24h":
-  //       return day;
-  //     case "7d":
-  //       return week;
-  //     case "1y":
-  //       return year;
-  //     default:
-  //       return day;
-  //   }
-  // };
-
-  useEffect(() => {
-    if (chartRef && chartRef.current && detail) {
-      const chartInstance = new Chartjs(chartRef.current, {
-        type: "line",
-        data: {
-          datasets: [
-            {
-              label: `${detail.name} price`,
-              data: day,
-              backgroundColor: "rgba(174, 305, 194, 0.5)",
-              borderColor: "rgba(174, 305, 194, 0.4",
-              pointRadius: 0,
-              fill: false,
-            },
-          ],
-        },
-        options: {
-          lineHeightAnnotation: {
-            always: true,
-            hover: false,
-            lineWeight: 1.5,
-          },
-
-          animation: {
-            duration: 2000,
-          },
-          maintainAspectRatio: false,
-          responsive: true,
-          scales: {
-            xAxes: [
-              {
-                type: "time",
-                distribution: "linear",
-              },
-            ],
-          },
-        },
-      });
+  const determineTimeFormat = () => {
+    switch (timeFormat) {
+      case "24h":
+        return day;
+      case "7d":
+        return week;
+      case "1y":
+        return year;
+      default:
+        return day;
     }
-  });
+  };
+
+  // useEffect(() => {
+  //   if (chartRef && chartRef.current && detail) {
+  //     const chartInstance = new Chartjs(chartRef.current, {
+  //       type: "line",
+  //       data: {
+  //         datasets: [
+  //           {
+  //             label: `${detail.name} price`,
+  //             data: determineTimeFormat(),
+  //             backgroundColor: "white",
+  //             borderColor: "red",
+  //             pointRadius: 1,
+  //             fill: false,
+  //           },
+  //         ],
+  //       },
+  //       options: {
+  //         lineHeightAnnotation: {
+  //           always: true,
+  //           hover: true,
+  //           lineWeight: 1.5,
+  //         },
+
+  //         animation: {
+  //           duration: 2000,
+  //         },
+  //         maintainAspectRatio: true,
+  //         responsive: true,
+  //         scales: {
+  //           x: {
+  //             max: 5000,
+  //             min: 0,
+  //             type: "category",
+  //             labels: ["January", "February", "March", "April", "May", "June"],
+  //           },
+  //         },
+  //       },
+  //     });
+  //   }
+  // });
 
   const renderPrice = () => {
     if (detail) {
       return (
-        <>
-          <p className="my-0">${detail.current_price.toFixed(2)}</p>
+        <div className="details-price-wrapper">
+          <p className="my-0 details-price">
+            ${detail.current_price.toFixed(2)}
+          </p>
           <UpDown
             value={detail.price_change_percentage_24h.toFixed(2)}
             classe={"cryptos-chart__subtext"}
           ></UpDown>
-        </>
+        </div>
       );
     }
   };
+  const chart = {
+    datasets: [
+      {
+        label: ` price`,
+        data: determineTimeFormat(),
+        fill: false,
+        borderWidth: 2.5,
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgba(255, 99, 132, 0.8)",
+        pointRadius: 0,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "white",
+        pointHoverBorderColor: "white",
+        pointHoverBorderWidth: 5,
+        pointRotation: 90,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      xAxes: [
+        {
+          type: "time",
+          distribution: "linear",
+        },
+      ],
+    },
+  };
+
   return (
     <div className="container">
       <div>{renderPrice()}</div>
+      {/* <div>
+        <canvas ref={chartRef} id="myChart" width={400} height={400}></canvas>
+      </div> */}
       <div>
-        <canvas ref={chartRef} id="myChart" width={250} height={250}></canvas>
+        <Line data={chart} options={options} height="450" width="700" />
       </div>
       <div className="chart-button mt-1">
         <button
