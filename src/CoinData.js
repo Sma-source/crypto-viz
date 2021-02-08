cd7a4e2d773e6ffaae0d4705de3213192e086d71
Sma-source <rabhi.smain@gmail.com>
2021-02-08 23:26:23 +0100

    render data

--- src/CoinData.js
+++ src/CoinData.js
@@ -2,20 +2,33 @@ import React from "react";
 
 const CoinData = ({ data }) => {
   const { detail } = data;
-  return (
-    <>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">hello</div>
-          <div className="col-md-4">hello</div>
-          <div className="col-md-4">hello</div>
-          <div className="col-md-4">hello</div>
-          <div className="col-md-4">hello</div>
-          <div className="col-md-4">hello</div>
+  if (detail) {
+    return (
+      <>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              Market Cap:
+              <p>{detail.market_cap}</p>
+            </div>
+            <div className="col-md-4">
+              24h Low / 24h High:
+              <p>
+                ${detail.low_24h} / ${detail.high_24h}{" "}
+              </p>
+            </div>
+            <div className="col-md-4">
+              Circulating Supply:
+              <p>{detail.circulating_supply}</p>
+            </div>
+            <div className="col-md-4">hello</div>
+            <div className="col-md-4">hello</div>
+            <div className="col-md-4">hello</div>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };
 
 export default CoinData;
