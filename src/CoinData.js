0d8a750c548f1cecd8cccac4ef771e5087f308f9
Sma-source <rabhi.smain@gmail.com>
2021-02-09 22:02:54 +0100

    update Coindata.js: add grid

--- src/CoinData.js
+++ src/CoinData.js
@@ -5,25 +5,48 @@ const CoinData = ({ data }) => {
   if (detail) {
     return (
       <>
-        <div className="container">
+        <div className="coindata">
           <div className="row">
-            <div className="col-md-4">
-              Market Cap:
-              <p>{detail.market_cap}</p>
+            <div className="col-sm-4 col-xs-12">
+              <div className="coindata-cards">
+                <div className="coindata-cards_background">
+                  <div className="coindata-cards_content">
+                    <div className="coindata-cards_img coindata-cards_img--oz">
+                      Market Cap:
+                      <p>{detail.market_cap}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="col-md-4">
-              24h Low / 24h High:
-              <p>
-                ${detail.low_24h} / ${detail.high_24h}{" "}
-              </p>
+
+            <div className="col-sm-4 col-xs-12">
+              <div className="coindata-cards">
+                <div className="coindata-cards_background">
+                  <div className="coindata-cards_content">
+                    <div className="coindata-cards_img coindata-cards_img--oz">
+                      24h Low / 24h High:
+                      <p>
+                        ${detail.low_24h} / ${detail.high_24h}{" "}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="col-md-4">
-              Circulating Supply:
-              <p>{detail.circulating_supply}</p>
+
+            <div className="col-sm-4 col-xs-12">
+              <div className="coindata-cards">
+                <div className="coindata-cards_background">
+                  <div className="coindata-cards_content">
+                    <div className="coindata-cards_img coindata-cards_img--oz">
+                      Circulating Supply:
+                      <p>{detail.circulating_supply}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="col-md-4">hello</div>
-            <div className="col-md-4">hello</div>
-            <div className="col-md-4">hello</div>
           </div>
         </div>
       </>
