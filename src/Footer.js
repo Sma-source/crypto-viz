c11f648153afba8a59a267c0d8424c049a5bbd86
Sma-source <rabhi.smain@gmail.com>
2021-02-10 23:10:07 +0100

    create footer component

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <div className="container-large">
+        <div className="bottom">
+          <label className="small">© 2020 Compound Labs, Inc.</label>
+          <div className="social"></div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
