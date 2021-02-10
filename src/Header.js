d8bd88677c0260663c7706964bcc535f2c130097
Sma-source <rabhi.smain@gmail.com>
2021-02-10 16:24:04 +0100

    create Header.js component

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header id="header">
+      <div className="container-large">
+        <div className="row align-middle">
+          <div className="col-sm-3 col-xs-10 logo">
+            <a href="#" className="brand"></a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
