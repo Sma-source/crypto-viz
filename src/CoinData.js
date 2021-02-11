70292b84029de7c2d5edf9e1a4c882fdba9b3ed4
Sma-source <rabhi.smain@gmail.com>
2021-02-11 22:25:12 +0100

    update: render new data

--- src/CoinData.js
+++ src/CoinData.js
@@ -1,47 +1,55 @@
 import React from "react";
+import Details from "./pages/Details";
 
 const CoinData = ({ data }) => {
   const { detail } = data;
   if (detail) {
     return (
       <>
-        <div className="coindata">
-          <div className="row">
-            <div className="col-sm-4 col-xs-12">
-              <div className="coindata-cards">
-                <div className="coindata-cards_background">
-                  <div className="coindata-cards_content">
-                    <div className="coindata-cards_img coindata-cards_img--oz">
-                      Market Cap:
-                      <p>{detail.market_cap}</p>
-                    </div>
+        <div className="container">
+          <div className="content">
+            <div className="row">
+              <div className="col-sm-6 coindata">
+                <div className="panel">
+                  <div className="panel-header">
+                    <h4>Market Details</h4>
                   </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-sm-4 col-xs-12">
-              <div className="coindata-cards">
-                <div className="coindata-cards_background">
-                  <div className="coindata-cards_content">
-                    <div className="coindata-cards_img coindata-cards_img--oz">
-                      24h Low / 24h High:
-                      <p>
-                        ${detail.low_24h} / ${detail.high_24h}{" "}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-sm-4 col-xs-12">
-              <div className="coindata-cards">
-                <div className="coindata-cards_background">
-                  <div className="coindata-cards_content">
-                    <div className="coindata-cards_img coindata-cards_img--oz">
-                      Circulating Supply:
-                      <p>{detail.circulating_supply}</p>
+                  <div className="content">
+                    <div className="form">
+                      <div className="calculation">
+                        <span className="description">Price</span>
+                        <span className="value">${detail.current_price} </span>
+                      </div>
+                      <div className="calculation">
+                        <span className="description">Market Cap</span>
+                        <span className="value">{detail.market_cap} </span>
+                      </div>
+                      <div className="calculation">
+                        <span className="description">Total Volume</span>
+                        <span className="value">{detail.total_volume} </span>
+                      </div>
+                      <div className="calculation">
+                        <span className="description">Circulate Supply</span>
+                        <span className="value">
+                          {detail.circulating_supply}{" "}
+                        </span>
+                      </div>
+                      <div className="calculation">
+                        <span className="description">High 24h</span>
+                        <span className="value">${detail.high_24h} </span>
+                      </div>
+                      <div className="calculation">
+                        <span className="description">Low 24h</span>
+                        <span className="value">${detail.low_24h} </span>
+                      </div>
+                      <div className="calculation">
+                        <span className="description">All Time High</span>
+                        <span className="value">${detail.ath} </span>
+                      </div>
+                      <div className="calculation">
+                        <span className="description">All Time Low</span>
+                        <span className="value">${detail.atl} </span>
+                      </div>
                     </div>
                   </div>
                 </div>
