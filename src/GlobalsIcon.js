a4c082c949c68e93d829ad97aca15d9c0aed7cd3
Sma-source <rabhi.smain@gmail.com>
2021-02-15 22:13:52 +0100

    create: GlobalsIcon component

---
+++ src/GlobalsIcon.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { FaChartLine } from "react-icons/fa";
+
+const GlobalsIcon = () => {
+  return (
+    <>
+      <div className="col-sm-6">
+        <div className="iconglobals">
+          <FaChartLine className="GlobalsIcon" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default GlobalsIcon;
