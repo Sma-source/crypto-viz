4b980342bce9e387b792cbb07ac4c2bfe2e0f0a8
Sma-source <rabhi.smain@gmail.com>
2021-02-15 19:44:59 +0100

    update Hero.js: create title and cta button

--- src/Hero.js
+++ src/Hero.js
@@ -4,7 +4,12 @@ const Hero = () => {
   return (
     <section className="hero">
       <div className="container">
-        <h2>Aperçu des marchés</h2>
+        <div className="col-sm-6 coindata text-center">
+          <h3>CoinData</h3>
+          <h2 className="titre">Info, graphique et Prix des Cryptomonnaies</h2>
+          <h3>Info, graphique et Prix des Cryptomonnaies</h3>
+          <button>Commencer maintenant</button>
+        </div>
       </div>
     </section>
   );
