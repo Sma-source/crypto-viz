495b8d11f89d5aaca6b60b0f7d89ad22f93f88e4
Sma-source <rabhi.smain@gmail.com>
2021-02-18 17:59:10 +0100

    add function getnumber to get moneyformat in js

--- src/CoinData.js
+++ src/CoinData.js
@@ -2,12 +2,35 @@ import React from "react";
 
 const CoinData = ({ data }) => {
   const { detail } = data;
+
+  function getNumber(num) {
+    const units = ["M", "B", "T", "Q"];
+    const unit = Math.floor((num / 1.0e1).toFixed(0).toString().length);
+    const r = unit % 3;
+    const x = Math.abs(Number(num)) / Number("1.0e+" + (unit - r)).toFixed(2);
+    return x.toFixed(2) + units[Math.floor(unit / 3) - 2];
+  }
   if (detail) {
     return (
       <>
         <div className="container">
           <div className="content">
             <div className="row">
+              <div className="col-sm-6 coindata">
+                <div className="panel">
+                  <div className="panel-header">
+                    <h4>Title</h4>
+                  </div>
+                  <div className="content">
+                    <div className="form">
+                      <div className="calculation">
+                        <span className="description">texte</span>
+                        <span className="value">Number</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <div className="col-sm-6 coindata">
                 <div className="panel">
                   <div className="panel-header">
@@ -23,18 +46,23 @@ const CoinData = ({ data }) => {
                         <span className="description">
                           Capitalisation boursière
                         </span>
-                        <span className="value">{detail.market_cap} </span>
+                        <span className="value">
+                          ${getNumber(detail.market_cap)}{" "}
+                        </span>
                       </div>
                       <div className="calculation">
                         <span className="description">Volume total</span>
-                        <span className="value">{detail.total_volume} </span>
+                        <span className="value">
+                          ${getNumber(detail.total_volume)}{" "}
+                        </span>
                       </div>
                       <div className="calculation">
                         <span className="description">
                           Approvisionnement en circulation
                         </span>
                         <span className="value">
-                          {detail.circulating_supply}{" "}
+                          {getNumber(detail.circulating_supply)} &nbsp;
+                          {detail.symbol}
                         </span>
                       </div>
                       <div className="calculation">
