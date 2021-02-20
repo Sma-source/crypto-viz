0061edae07fe5c30f76f79cc9cbc5f2a64fabc26
Sma-source <rabhi.smain@gmail.com>
2021-02-20 14:54:01 +0100

    change text

--- src/Hero.js
+++ src/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
     <section className="hero">
       <div className="container">
         <div className="col-sm-6 coindata text-center">
-          <h2 className="titre">Info, graphique et Prix des Cryptomonnaies</h2>
+          <h2 className="titre">Info, Graphique et Prix des Cryptomonnaies</h2>
           <h3>Rester informé du cours de vos crypto préférés</h3>
           <Link to="#" className="bouton">
             Commencer maintenant
